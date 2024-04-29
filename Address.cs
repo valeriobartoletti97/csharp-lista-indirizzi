9ae83b2f4ad88022cd96148f540e4839c6409b97
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-29 14:41:41 +0200

    first read of addresses file

---
+++ Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Address
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public int Zip { get; set; }
+
+        public Address(string name, string surname, string street, string city, string province, int zip) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C://Users//Programmazione//Downloads/addresses.csv";
+            GetAddress(path);
+        }
+
+        public static List<Address> GetAddress(string path) 
+        {
+            List<Address> addresses = new List<Address>();
+            var stream = File.OpenText(path);
+            int counter = 0;
+
+            while (!stream.EndOfStream)
+            {
+                var line = stream.ReadLine();
+                Console.WriteLine(line);
+
+                counter++;
+                if (counter <= 1)
+                    continue;
+                try
+                {
+                    var info = line.Split(",");
+                    string name = info[0];
+                    string surname = info[1];
+                    string street = info[2];
+                    string city = info[3];
+                    string province = info[4];
+                    int zip = int.Parse(info[5]);
+
+                    Address address = new Address(name, surname, street, city, province, zip);
+                    addresses.Add(address);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+            stream.Close();
+            return addresses;
         }
     }
 }
