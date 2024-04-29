bf7d1050f4e8a81cde34bb6c08ff5ba805218d4b
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-04-29 15:53:20 +0200

    get all addresses and created added exception errors

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             string path = "C://Users//Programmazione//Downloads/addresses.csv";
             GetAddress(path);
+
         }
 
         public static List<Address> GetAddress(string path) 
@@ -24,7 +25,21 @@
                     continue;
                 try
                 {
-                    var info = line.Split(",");
+                    string[] info = line.Split(",");
+                    if (info.Length >= 7)
+                    {
+                        //Removes the element in position [2] in an array
+                        info = info.Where((val, idx) => idx != 2).ToArray();
+                    }
+                    if (info.Length < 6)
+                    {
+                        throw new Exception("Some information is missed!");
+                    }
+                    if (info.Contains(""))
+                    {
+                        throw new Exception("There shouldn't be empty fields!");
+
+                    }
                     string name = info[0];
                     string surname = info[1];
                     string street = info[2];
@@ -37,7 +52,7 @@
                 }
                 catch(Exception e)
                 {
-                    Console.WriteLine(e.Message);
+                    Console.WriteLine($"Error: {e.Message} {e.StackTrace}");
                 }
                 
             }
